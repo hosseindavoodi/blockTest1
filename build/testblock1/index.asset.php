a0497a18d5c4aeade0b78a27c06ce9bb13fd3406
Hossein Davoodi <H.Davoodi@trenkwalder.com>
2025-10-20 10:23:04 +0330

    initial upload

---
+++ build/testblock1/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '76a081e01d681e0a3739');
---
+++ build/testblock1/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '52b50cfcf7e39e7888dc');
